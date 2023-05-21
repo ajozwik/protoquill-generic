805351272428275b45a99c40e0961de717516c4f
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2023-05-21 20:16:40 +0200

    add coverage

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# +publishLocalSigned
+
+. $HOME/.sbt/.protoquill-generic-coverage
+sbt8 -Dquill.macro.log=false clean +coverage +test +coverageAggregate && sbt8 +coveralls || exit 1
 PATH=$HOME/bin:$PATH sbt8 -Dquill.macro.log=false clean test publishLocalSigned publishSigned sonatypeRelease
