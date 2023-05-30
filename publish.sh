708b7db9efd52399fb5a776332c6d1f5865cfeeb
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2023-05-30 22:55:56 +0200

    codecov

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . $HOME/.sbt/.protoquill-generic-coverage
-sbt8 -Dquill.macro.log=false clean +coverage +test +coverageAggregate && sbt8 +coveralls || exit 1
+sbt8 -Dquill.macro.log=false clean coverage test coverageAggregate && sbt8 +coveralls || exit 1
 PATH=$HOME/bin:$PATH sbt8 -Dquill.macro.log=false clean test publishLocalSigned publishSigned sonatypeRelease
