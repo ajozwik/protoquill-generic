b83dbb03336c6fd88938337d5e64bfaf4bd0838a
ajozwik <andrzej.jozwik@gmail.com>
2026-06-23 09:27:22 +0200

    remove comment

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#sbt -Dquill.macro.log=false clean test publishLocalSigned publishSigned sonaUpload sonaRelease
-
 sbt -Dquill.macro.log=false publishLocalSigned publishSigned sonaUpload sonaRelease
